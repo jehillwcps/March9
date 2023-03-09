a0d0e2c1ade7bb64ebca2e0c437a62044e13eafd
Julia Hill <jehill@worcesterk12.org>
2023-03-09 09:27:10 -0500

    Changed globe to person

--- March9/ContentView.swift
+++ March9/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, Julia!")
