bafb52a5b5802da6c3e6ba219782cf3e1680cf42
Julia Hill <jehill@worcesterk12.org>
2023-03-09 09:35:57 -0500

    Added "Have a lovely day!"

--- March9/ContentView.swift
+++ March9/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, Julia!")
+            Text("Have a lovely day!")
         }
         .padding()
     }
