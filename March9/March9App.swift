f478737fa135503af11fff82ea7adce1d01b31df
Julia Hill <jehill@worcesterk12.org>
2023-03-09 09:05:32 -0500

    Initial Commit

---
+++ March9/March9App.swift
@@ -0,0 +1,17 @@
+//
+//  March9App.swift
+//  March9
+//
+//  Created by Julia Hill on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct March9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
